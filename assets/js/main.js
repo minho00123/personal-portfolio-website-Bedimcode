ce1061005d0cbe987bc2d449419af119dc566062
Min Ho Jang <mino@Mins-MacBook-Air.local>
2023-05-22 14:46:48 +0900

    feat: add Contact Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,6 +40,37 @@ const blurHeader = () => {
 window.addEventListener("scroll", blurHeader);
 
 /*=============== EMAIL JS ===============*/
+// Before proceeding, I recommend creating your EmailJs account(https://www.emailjs.com/)
+const contactForm = document.getElementById("contact-form"),
+  contactMessage = document.getElementById("contact-message");
+
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  // serviceID - templateID - #form - publicKey
+  emailjs
+    .sendForm("serviceID", "templateID", "#contact-form", "publicKey")
+    .then(
+      () => {
+        // Show sent message
+        contactMessage.textContent = "Message sent successfully ✅";
+
+        // Remove message after five seconds
+        setTimeout(() => {
+          contactMessage.textContent = "";
+        }, 5000);
+
+        // Clear input fields
+        contactForm.reset();
+      },
+      () => {
+        // Show error message
+        contactMessage.textContent = "Message not sent (service error) ❌";
+      }
+    );
+};
+
+contactForm.addEventListener("submit", sendEmail);
 
 /*=============== SHOW SCROLL UP ===============*/
 
